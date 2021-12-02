d1f7d26b66d23ba08423e9a8eee63b40f5005f03
fairuzanjab15 <90047187+fairuzanjab15@users.noreply.github.com>
2021-12-02 10:44:19 +0700

    Add files via upload

---
+++ Helicopter.py
@@ -0,0 +1,26 @@
+import time
+
+#Helicopter by anjab, inspired from helicopter meme
+
+while True:
+    sleep = 0.3 
+
+    print ("    :PVP:PVP:LOL:         ")
+    print ("             |||          ")
+    print ("          __/ n \___      ")
+    print (" ||    __/  |   |   \__   ")
+    print (":||:=====___|   |______\  ")
+    print ("           -|---|-        ")
+    time.sleep(sleep)
+    print('\033c')
+    
+
+    print ("            :LOL:PVP:PVP  ")
+    print ("             |||          ")
+    print ("          __/ n \___      ")
+    print ("       __/  |   |   \__   ")
+    print (":||:=====___|   |______\  ")
+    print (" ||        -|---|-        ")
+    time.sleep(sleep) 
+    print('\033c')
+    
